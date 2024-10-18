dd6ba0f3e83003508c7681c39befd6e32b3f5a29
Marina Ivanova <m.ivanova@innopolis.university>
2024-10-19 00:42:15 +0300

    setup styles

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import WebsitesDashboard from "./routes/WebsitesDashboard/WebsitesDashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <WebsitesDashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/routes/WebsitesDashboard/WebsitesDashboard.tsx
@@ -0,0 +1,18 @@
+import "./WebsitesDashboard.css";
+
+function WebsitesDashboard() {
+  return (
+    <div className="WebsitesDashboard">
+      {/* <h1>H1 abcdefg</h1>
+      <h2>H2 abcdefg</h2>
+      <h3>H3 abcdefg</h3>
+      <p>p abcdefg</p>
+      <p className="info-text">p abcdefg</p>
+      <p className="warning-text">p abcdefg</p>
+      <p className="button-text">p abcdefg</p>
+      <code>Hello, World!</code> */}
+    </div>
+  );
+}
+
+export default WebsitesDashboard;
