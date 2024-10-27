0e3752cf856771f4be6386a657bc80f613c269b9
NikitaKalitov <162014214+NikitaKalitov@users.noreply.github.com>
2024-10-27 12:41:24 +0300

    add zustand for state management
    add login page loader
    add userStore for managing user data
    add cookiesStore for managing cookies data
    add user interfaces

--- web/src/routes/login/LoginPage.tsx
+++ web/src/routes/login/LoginPage.tsx
@@ -1,4 +1,15 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from "react";
+import { redirect } from "react-router-dom";
+import { useUserStore } from "../../stores/userStore";
+
+export async function loader() {
+  const isLoggedIn = useUserStore.getState().isLoggedIn;
+  if (isLoggedIn()) {
+    return redirect("/");
+  }
+  return null;
+}
 
 export const LoginPage = () => {
   return <h1>LoginPage</h1>;
---
+++ web/src/stores/userStore.ts
@@ -0,0 +1,63 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import { IUser } from "../types/user";
+
+interface IUserStore {
+  user: IUser | null;
+
+  getDataFromCookies: () => void;
+  isLoggedIn: () => boolean;
+  login: ({ user }: { user: IUser }) => Promise<void>;
+  logout: () => void;
+}
+
+const userValue: IUser = {
+  email: "example@example.com",
+  token: "token",
+  refreshToken: "refreshToken",
+  tokenExpiresMilliseconds: 1000,
+  role: 1,
+};
+
+export const useUserStore = create<IUserStore>()(
+  persist(
+    (set, get) => ({
+      user: userValue,
+
+      getDataFromCookies: () => {},
+
+      isLoggedIn: () => {
+        if (
+          !get().user &&
+          get().user?.email === "" &&
+          get().user?.token === ""
+        ) {
+          return false;
+        } else {
+          return true;
+        }
+      },
+
+      login: async ({ user }) => {
+        set({ user: user });
+      },
+
+      logout: () => {
+        set({
+          user: {
+            email: "",
+            refreshToken: "",
+            role: -1,
+            token: "",
+            tokenExpiresMilliseconds: -1,
+          },
+        });
+      },
+    }),
+    {
+      name: "user-store",
+      storage: createJSONStorage(() => sessionStorage),
+      partialize: (state) => ({}),
+    }
+  )
+);
