f1d9fe9203bb3d814f6ba206d55afd9cbb509bb2
Antonyan Artem <100195487+Rus8921@users.noreply.github.com>
2024-11-01 19:11:53 +0300

    change dockerfile,and api

--- api/api.go
+++ api/api.go
@@ -2,29 +2,29 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"gitlab.pg.innopolis.university/antiddos/nginx-admin-panel-backend.git/cmd/backend"
-	"gitlab.pg.innopolis.university/antiddos/nginx-admin-panel-backend.git/cmd/backend/models"
-	"net/http"
+	//"gitlab.pg.innopolis.university/antiddos/nginx-admin-panel-backend.git/cmd/backend"
+	//"gitlab.pg.innopolis.university/antiddos/nginx-admin-panel-backend.git/cmd/backend/models"
+	//"net/http"
 )
 
 func main() {
 
-	backend.InitDb()
+	//backend.InitDb()
 	router := gin.Default()
 	user := router.Group("/user")
 	{
-		user.GET("/users")
-		user.POST("/login", func(context *gin.Context) {
-			var user models.User
-			if err := context.ShouldBind(&user); err != nil {
-				context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-				return
-			}
-			if err := models.CreateUser(backend.Db, &user); err != nil {
-				context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			}
-			context.JSON(http.StatusOK, gin.H{"data": user})
-		})
+		//user.GET("/users")
+		//user.POST("/login", func(context *gin.Context) {
+		//	var user models.User
+		//	if err := context.ShouldBind(&user); err != nil {
+		//		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		//		return
+		//	}
+		//	if err := models.CreateUser(backend.Db, &user); err != nil {
+		//		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		//	}
+		//	context.JSON(http.StatusOK, gin.H{"data": user})
+		//})
 		user.PUT("/users")
 		user.DELETE("/users")
 		user.POST("/registration")
