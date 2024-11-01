150453cea0bd0ebe47cddc1642e1d316629b368f
Marina Ivanova <m.ivanova@innopolis.university>
2024-11-01 23:01:27 +0300

    configure jest coverage report

--- web/jest.config.js
+++ web/jest.config.js
@@ -2,6 +2,21 @@
 module.exports = {
   testEnvironment: "node",
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    "^.+.tsx?$": ["ts-jest", {}],
   },
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    './src/**/*.ts?(x)',
+    '!./src/containers/dashboadr.tsx',
+    '!./src/index.tsx',
+    '!./src/containers/login/index.ts'
+  ],
+  coverageDirectory: "<rootDir>/coverage",
+  coverageReporters: [
+    ['html', {
+      subdir: 'html'
+    }]
+  ],
+  preset: 'ts-jest',
 };
